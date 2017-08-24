15e1ead6798ae82984e63cfee120d8ec5bfb6325
ZachySwag <zman000@icloud.com>
2017-08-24 19:24:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ bot.on("ready", function() {
 });
 
 bot.on("guildMemberAdd", function(member) {
-  member.guild.channels.find("name", "general-chat").sendMessage(member.toString() + " Welcome to the Official Inkopolis Heroes Discord! We're glad to have you here! Type \"$help\" if you need anything. Please read #rules so you'll be good to go. You have been givem the role of Non-Member, and you have basic permissions. If you would like to be a Member, just let an Admin know if you play Splatoon 1 or 2. If you don't, you will stay as a Non-Member. Thanks for joining the Discord! You'll be happy you joined!")
+  member.guild.channels.find("name", "general-chat").sendMessage(member.toString() + " Welcome to the Official Inkopolis Heroes Discord! We're glad to have you here! Type \"$help\" if you need anything. Please read " + channels.rules + " so you'll be good to go. You have been givem the role of Non-Member, and you have basic permissions. If you would like to be a Member, just let an Admin know if you play Splatoon 1 or 2. If you don't, you will stay as a Non-Member. Thanks for joining the Discord! You'll be happy you joined!")
 
   member.addRole(member.guild.roles.find("name", "(Non-Members) Salmonoid Chum"));
 });
@@ -27,8 +27,33 @@ bot.on ("message", function(message) {
     case "hello":
       message.channel.sendMessage("Hey " + message.author.toString() + "! What's up?? Having a good day? :thinking:");
       break;
-    case "memes":
-      message.channel.sendMessage("england is my city");
+    case "meme":
+      memess = Math.floor(Math.random() * 5) + 1;
+      if (memess == 1) {
+        message.channel.sendMessage(message.author.toString() + " England is my city. Wait is it a city? Let me check my math real quick...");
+      } else if (memess == 2) {
+        message.channel.sendMessage(message.author.toString() + " Mayonaise actually IS an instrument. You just gotta slurp it off the spoon!");
+      } else if (memess == 3) {
+        message.channel.sendMessage(message.author.toString() + " Why talk about memes you horific person! Harambe is dead! ;-;");
+      } else if (memess == 4) {
+        message.channel.sendMessage(message.author.toString() + " Cashhh meh ousside or you'll get the ban hamma bro. :P");
+      } else if (memess == 5) {
+        message.channel.sendMessage(message.author.toString() + " We are number on--- oh that meme's dead? Nevermind...");
+      } else if (memess == 6) {
+        message.channel.sendMessage(message.author.toString() + " That awkward moment when you get into the van, but there's no candy...");
+      }
+      break;
+    case "roastme":
+      roast = Math.floor(Math.random() * 4) + 1;
+      if (roast == 1) {
+        message.channel.sendMessage(message.author.toString() + " The best part about hanging out with you, is when I'm not.");
+      } else if (roast == 2) {
+        message.channel.sendMessage(message.author.toString() + " Were you born on the highway? That's where accidents happen.");
+      } else if (roast == 3) {
+        message.channel.sendMessage(message.author.toString() + " Sorry I'm not assuming your gender, I'm proving your gender.");
+      } else if (roast == 4) {
+        message.channel.sendMessage(message.author.toString() + " You're almost as annoying as Nintapple... not quite though...");
+      }
       break;
     case "info":
       message.channel.sendMessage(message.author.toString() + " I am the very best bot, like no bot ever was. I was programmed by a boy who caught them all. (No not Ash Ketchum)");
@@ -36,9 +61,10 @@ bot.on ("message", function(message) {
     case "help":
       message.channel.sendMessage(message.author.toString() + " Here is a list of commands:");
       var embed = new Discord.RichEmbed()
-        .addField("$hello:", "Say hi to me!")
+        .addField("$hello:", "Say hi to the bot!")
         .addField("$info:", "Find out who I am!")
-        .addField("$memes:", "Get a free meme! fresh and hot!");
+        .addField("$memes:", "Get a free meme! fresh and hot!")
+        .addField("$roastme:", "Have fun losing your self-esteem...");
       message.channel.sendEmbed(embed);
       break;
     default:
